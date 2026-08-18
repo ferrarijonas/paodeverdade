d88dd8c299bbfb7b5da81ac97512c3b757504d46
ferrarijonas <ferrarijonas@users.noreply.github.com>
2026-08-18 15:17:46 -0300

    Conecta formulário de inscrição ao Web App funcional

--- assets/js/inscricao-config.js
+++ assets/js/inscricao-config.js
@@ -2,5 +2,5 @@
    Troque WEB_APP_URL pela URL do seu Web App (Google Apps Script)
    depois de fazer o deploy (ver passo a passo). */
 var PDV_CONFIG = {
-  WEB_APP_URL: 'COLE_AQUI_A_URL_DO_WEB_APP'
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxHyN1-ZU49ZH4wU92MHw8PlgLayxVSiszJ7iGG47jdampR5CrTDK2hmo4OH0koPaKRtQ/exec'
 };
